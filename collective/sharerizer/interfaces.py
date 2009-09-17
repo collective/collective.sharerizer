199282a88e91a277d0540c73c970102e71f33b4f
David Glick <davidglick@groundwire.org>
2009-09-17 23:57:48 +0000

    fix browser layer precedence
    svn path=/collective.sharerizer/trunk/; revision=97710

--- collective/sharerizer/interfaces.py
+++ collective/sharerizer/interfaces.py
@@ -1,6 +1,6 @@
-from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
 
-class ISharerizerBrowserLayer(Interface):
+class ISharerizerBrowserLayer(IDefaultPloneLayer):
     """Marker applied to the request during traversal of sites that
        have collective.sharerizer installed
     """
