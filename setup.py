0fd0277c1c18510315b8cb7e04fab76f70124e94
Maurits van Rees <maurits@vanrees.org>
2012-11-27 14:13:25 +0000

    cleanup setup.py
    svn path=/collective.sharerizer/trunk/; revision=251776

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ version = '1.3dev'
 setup(name='collective.sharerizer',
       version=version,
       description="A package to inject Web 2.0 style sharing buttons into Plone's document_actions.",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.txt").read()),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='plone',
       author='Groundwire',
       author_email='jonb@groundwire.org',
